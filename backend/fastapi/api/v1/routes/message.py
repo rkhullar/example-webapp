8aeff5af6db8452a5ad465eb327c26d717eb5871
Rajan Khullar <rkhullar03@gmail.com>
2023-06-11 14:13:31 -0400

    adopt GetUser in codebase; isort

--- backend/fastapi/api/v1/routes/message.py
+++ backend/fastapi/api/v1/routes/message.py
@@ -1,18 +1,17 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import HTTPException, status
 
 from ...core.router import APIRouter
 from ...model.message import Message
-from ..depends import atlas, get_user
+from ..depends import GetUser, atlas
 from ..schema.crud import ListResponse
 from ..schema.message import CreateMessage
-from ..schema.user import User
 
 router = APIRouter()
 MessageAdapter = atlas(name='message', model=Message)
 
 
 @router.get('', response_model=ListResponse[Message])
-async def list_messages(adapter: MessageAdapter, user: User = Depends(get_user)):
+async def list_messages(user: GetUser, adapter: MessageAdapter):
     require_admin_role: bool = False
     if require_admin_role and 'internal/example/admins' not in user.groups:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
@@ -20,7 +19,7 @@ async def list_messages(adapter: MessageAdapter, user: User = Depends(get_user))
 
 
 @router.post('', response_model=Message)
-async def create_message(adapter: MessageAdapter, create_object: CreateMessage, user: User = Depends(get_user)):
+async def create_message(user: GetUser, adapter: MessageAdapter, create_object: CreateMessage):
     to_insert = create_object.to_pymongo(user_id=user.id)
     response = adapter.collection.insert_one(to_insert)
     return Message(
