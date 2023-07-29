57ea1eb4e21adc879d5c0db996e19e260708fbf1
Rajan Khullar <rkhullar03@gmail.com>
2023-07-29 12:02:33 -0400

    save

--- backend/fastapi/api/model/document.py
+++ backend/fastapi/api/model/document.py
@@ -2,19 +2,18 @@ from __future__ import annotations
 
 from typing import Generic, TypeVar
 
-from pydantic import BaseModel, Field, RootModel
+from pydantic import BaseModel, RootModel
 
 from .object_id import PydanticObjectId
 
 
 class Document(BaseModel):
-    id: PydanticObjectId | str = Field(alias='_id')
+    id: PydanticObjectId | str
 
     @classmethod
     def from_pymongo(cls, doc: dict) -> DocumentType:
-        # _id = doc.pop('_id')
-        # return cls(id=_id, **doc)
-        return cls(**doc)
+        _id = doc.pop('_id')
+        return cls(id=_id, **doc)
 
     @property
     def object_ref(self) -> ObjectRef[DocumentType]:
